b44bdeb3238b487749099674b00284008cb253c8
AimanFadil <aimanbello2001@gmail.com>
2024-01-10 17:18:07 +0100

    contCards done

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -14,6 +14,14 @@ export default {
 </script>
 <template lang="">
     <main>
+        <div class="diff">
+            <select class="form-select" aria-label="Default select example">
+                <option selected>Alien</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+        </div>
         <contCards/>
     </main>
 </template>
@@ -24,8 +32,13 @@ export default {
 
 main {
     background-color: rgba(212, 143, 56, 255);
-    height: 100vh;
+    height: 100%;
 
-
+    .diff {
+        background-color: rgba(212, 143, 56, 255);
+        width: 200px;
+        padding: 20px;
+        height: 150px;
+    }
 }
 </style>
--- src/components/contCards.vue
+++ src/components/contCards.vue
@@ -28,8 +28,12 @@ export default {
         <div class="row">
             <div class="col-12" >
                 <div class="content">
-                    <div class="cards text-center" v-for="card, index in store.cardsList[0].card_images" :key="index">
-                        <img :src="card.image_url" >
+                    <div class="cards text-center" v-for="card, index in store.cardsList" :key="index">
+                        <img :src="card.card_images[0].image_url" >
+                        <div class="testo">
+                            <p class="text-white">{{ card.name }}</p>
+                            <p>{{ card.archetype }}</p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -43,12 +47,25 @@ export default {
 
 .content {
     display: flex;
-    flex-direction: row;
+    justify-content: center;
     flex-wrap: wrap;
+    margin: 10px;
+
 
     .cards {
-        width: calc(100% / 5 - 10px);
-        padding: 20px;
+        width: calc(100% / 6 - 10px);
+        padding: 10px;
+        margin: 10px;
+
+        .testo {
+            background-color: rgba(212, 143, 56, 255);
+            height: 90px;
+        }
+
+        img {
+            width: 100%;
+        }
+
     }
 }
 </style>
