05cfb67fb92ce4d5fefd001a8108237f81abf20e
AimanFadil <aimanbello2001@gmail.com>
2024-01-10 16:38:50 +0100

    push di controllo

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -14,7 +14,7 @@ export default {
 </script>
 <template lang="">
     <main>
-       
+        <contCards/>
     </main>
 </template>
 
--- src/components/contCards.vue
+++ src/components/contCards.vue
@@ -1,17 +1,54 @@
 <script>
+import { store } from '../store.js';
+import axios from 'axios';
 export default {
     name: 'contCards',
     data() {
         return {
-
+            store,
         }
-    }
+    },
+    methods: {
+        getCardsList() {
+            axios.get(store.endpoint).then((response) => {
+                this.store.cardsList = response.data.data
+            })
+        }
+
+    },
+    created() {
+        this.getCardsList();
+
+    },
 }
 </script>
 <template lang="">
   <div>
-    
+    <div class="container bg-white ">
+        <div class="row">
+            <div class="col-12" >
+                <div class="content">
+                    <div class="cards text-center" v-for="card, index in store.cardsList[0].card_images" :key="index">
+                        <img :src="card.image_url" >
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../styles/generals.scss';
+
+.content {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+
+    .cards {
+        width: calc(100% / 5 - 10px);
+        padding: 20px;
+    }
+}
+</style>
